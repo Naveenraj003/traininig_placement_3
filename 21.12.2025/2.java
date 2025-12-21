5ade03d067407fd32d0ca72285e2792711914c19
Naveenraj003 <151421091+Naveenraj003@users.noreply.github.com>
2025-12-21 22:46:36 +0530

    Create 2.java

---
+++ 21.12.2025/2.java
@@ -0,0 +1,9 @@
+class LargestOfThree {
+    public static void main(String[] args) {
+        int a = 10, b = 25, c = 15;
+
+        int largest = (a > b && a > c) ? a : (b > c ? b : c);
+
+        System.out.println("Largest: " + largest);
+    }
+}
