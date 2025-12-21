e1d48f7806ca9f57179df3e58c06236e32cc610c
Naveenraj003 <151421091+Naveenraj003@users.noreply.github.com>
2025-12-21 22:49:36 +0530

    Create 3.java

---
+++ 21.12.2025/3.java
@@ -0,0 +1,12 @@
+class CountDigits {
+    public static void main(String[] args) {
+        int num = 12345, count = 0;
+
+        while (num != 0) {
+            count++;
+            num /= 10;
+        }
+
+        System.out.println("Number of Digits: " + count);
+    }
+}
